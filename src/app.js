3b7bf4bdd7ee692f0afdf95153cdf7356198e3ab
apple <apple@appledeMacBook-Pro.local>
2015-04-19 10:51:26 +0800

    One change

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
 
+var CONSTENT={
+    "DOT_NUM":30
+};
+
 var HelloWorldLayer = cc.Layer.extend({
     sprite:null,
     ctor:function () {
@@ -6,64 +10,6 @@ var HelloWorldLayer = cc.Layer.extend({
         // 1. super init first
         this._super();
 
-//        /////////////////////////////
-//        // 2. add a menu item with "X" image, which is clicked to quit the program
-//        //    you may modify it.
-//        // ask the window size
-//        var size = cc.winSize;
-//
-//        // add a "close" icon to exit the progress. it's an autorelease object
-//        var closeItem = new cc.MenuItemImage(
-//            res.CloseNormal_png,
-//            res.CloseSelected_png,
-//            function () {
-//                cc.log("Menu is clicked!");
-//            }, this);
-//        closeItem.attr({
-//            x: size.width - 20,
-//            y: 20,
-//            anchorX: 0.5,
-//            anchorY: 0.5
-//        });
-//
-//        var menu = new cc.Menu(closeItem);
-//        menu.x = 0;
-//        menu.y = 0;
-//        this.addChild(menu, 1);
-//
-//        /////////////////////////////
-//        // 3. add your codes below...
-//        // add a label shows "Hello World"
-//        // create and initialize a label
-//        var helloLabel = new cc.LabelTTF("Hello World", "Arial", 38);
-//        // position the label on the center of the screen
-//        helloLabel.x = size.width / 2;
-//        helloLabel.y = 0;
-//        // add the label as a child to this layer
-//        this.addChild(helloLabel, 5);
-//
-//        // add "HelloWorld" splash screen"
-//        this.sprite = new cc.Sprite(res.HelloWorld_png);
-//        this.sprite.attr({
-//            x: size.width / 2,
-//            y: size.height / 2,
-//            scale: 0.5,
-//            rotation: 180
-//        });
-//        this.addChild(this.sprite, 0);
-//
-//        this.sprite.runAction(
-//            cc.sequence(
-//                cc.rotateTo(2, 0),
-//                cc.scaleTo(2, 1, 1)
-//            )
-//        );
-//        helloLabel.runAction(
-//            cc.spawn(
-//                cc.moveBy(2.5, cc.p(0, size.height - 40)),
-//                cc.tintTo(2.5,255,125,0)
-//            )
-//        );
 
         //init space
         this.space = new cp.Space();
@@ -75,14 +21,21 @@ var HelloWorldLayer = cc.Layer.extend({
         this.addChild(circle,1,1);
         //circle.body.applyImpulse(cp.v(circle.x,circle.y),cp.v(300,400));
        // circle.runAction(cc.moveBy(2,cc.p(100,100)));
-        circle.body.setVel(cp.v(200,200));
+        circle.body.setVel(cp.v(0,200));
+        circle.body.applyForce(cp.v(0,100),cp.v(0,0));
 
 
-
-       // circle.body.setVel(cp.v(300,0));
         this.scheduleUpdate();
 
         this.endPoint = new cc.Sprite(res.dot_png);
+        this.dots={};
+        for(var i=0;i!=CONSTENT.DOT_NUM;i++){
+            this.dots[i]= new cc.Sprite(res.circle_png);
+            this.addChild(this.dots[i]);
+            this.dots[i].setPosition(cc.p(i*20,i*20));
+            this.dots[i].opacity=0;
+            this.dots[i].runAction(cc.scaleTo(1,0.5));
+        }
         this.addChild(this.endPoint);
 
     },
@@ -104,18 +57,30 @@ var HelloWorldLayer = cc.Layer.extend({
         target.endPoint.setPosition(this.dragOffsetStartX,this.dragOffsetStartY);
         target.unscheduleUpdate();
         target.scheduleUpdate();
-        target.simulateTrajectory(cp.v(this.dragOffsetStartX,this.dragOffsetStartY));
-        return false;
+
+        target.simulateTrajectory(target.getDirect(this.dragOffsetStartY/this.dragOffsetStartX));
+        return true;
     },
     onTouchMoved:function(touch,event){
         var target= event.getCurrentTarget();
+
         this.dragOffsetEndX = touch.getLocationX();
         this.dragOffsetEndY = touch.getLocationY();
+        target.endPoint.setPosition(this.dragOffsetEndX,this.dragOffsetEndY);
         this.dragDistanceX = this.dragOffsetStartX - this.dragOffsetEndX;
         this.dragDistanceY = this.dragOffsetStartY - this.dragOffsetEndY;
-        target.simulateTrajectory(cp.v(this.dragOffsetEndX,this.dragOffsetEndY));
+        target.simulateTrajectory(target.getDirect(this.dragOffsetEndY/this.dragOffsetEndX));
     },
-    onTouchEnded:function(touch,event){},
+    onTouchEnded:function(touch,event){
+        //project a bullet
+        var target= event.getCurrentTarget();
+
+        //hide trajectory
+        for(var i=0;i!=CONSTENT.DOT_NUM;i++){
+            target.dots[i].opacity =0;
+        }
+        //target.endPoint.setVel(100,100);
+    },
     simulateTrajectory:function(v){
        //init one body
         var body = new cp.Body(1, cp.momentForCircle(1,0,10,cp.v(0,0)));
@@ -124,39 +89,43 @@ var HelloWorldLayer = cc.Layer.extend({
         var shape = new cp.CircleShape(body,10,0);
         this.space.addShape(shape);
         body.setVel(v);
-        var dots=[];
 
         var c= this.getChildByTag(1);
         var cx = c.x, cy = c.y;
         var cvx = c.body.vx, cvy= c.body.vy;
-        //this.unscheduleUpdate();
 
-        for(var i=0;i!=79;i++){
-            dots[i] = new cc.Sprite(res.circle_png);
-            this.addChild(dots[i]);
-            this.space.step(this.deltaTime);
-            dots[i].setPosition(body.p.x,body.p.y);
+        for(var i=0;i!=CONSTENT.DOT_NUM;i++){
+            this.dots[i].opacity =255;
+            this.space.step(this.deltaTime*2);
+            this.dots[i].setPosition(body.p.x,body.p.y);
+
 
         }
         this.space.removeBody(body);
 
         c.setPosition(cc.p(cx,cy));
         c.body.setVel(cp.v(cvx,cvy));
-        //c.body.setVel(cp.v(200,200));
-        //this.scheduleUpdate();
+    },
+    drawDots:function(p){
+
+    },
+    // will set a speed to
+    getDirect:function(k){
+        var width= cc.winSize.width;
+        return cp.v(width,k * width);
     },
     update:function(dt){
 
         var c= this.getChildByTag(1);
 
-//        if(c.y>0){
-//
-//        cc.log(dt);
-//        cc.log(c);
-//        cc.log("vx:"+ c.body.vx + "vy:"+ c.body.vy);
-//        cc.log("px:"+c.body.p.x +"py"+ c.body.p.y);
-//            cc.log("spx:"+c.x +"spy"+ c.y);
-//        }
+        if(c.y>0){
+
+        //cc.log(c);
+         //   cc.log(c.body.f);
+        //cc.log("vx:"+ c.body.vx + "vy:"+ c.body.vy);
+        //cc.log("px:"+c.body.p.x +"py"+ c.body.p.y);
+         //   cc.log("spx:"+c.x +"spy"+ c.y);
+        }
         //cc.log(this.space.getCurrentTimeStep());
         this.deltaTime = dt;
         this.space.step(0.017);
--- src/physics.js
+++ src/physics.js
@@ -8,7 +8,7 @@ var Circle = cc.PhysicsSprite.extend({
         this._super(png);
         this.space = space;
         //set body
-        this.body = new cp.Body(1, cp.momentForCircle(1,0,10,cp.v(0,0)));
+        this.body = new cp.Body(10, cp.momentForCircle(10,0,10,cp.v(0,0)));
         //this.body.setPos (cc.p(100,100));
         //this.body.applyImpulse(cp.v(100,100),cp.v(300,300));
         this.space.addBody(this.body);
